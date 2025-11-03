e957782264325f8a1b7dbc6f344070cadfae8e76
thatkid02 <nelsondsouza02@gmail.com>
2025-11-03 21:19:02 +0530

    048: balanced binary tree dfs

--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,39 @@
 
 using namespace std;
 
-#include "code150/diameter_of_binary_tree.cpp"
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
+};
+
+#include "code150/balanced_binary_tree.cpp"
 
 int main() {
   Solution sol;
 
-  TreeNode* root = new TreeNode(1);
-  root->left = new TreeNode(2);
-  root->right = new TreeNode(3);
-  root->left->left = new TreeNode(4);
-  root->left->right = new TreeNode(5);
+  TreeNode* node7 = new TreeNode(7);
+  TreeNode* node15 = new TreeNode(15);
+  TreeNode* node20 = new TreeNode(20, node15, node7);
+  TreeNode* node9 = new TreeNode(9);
+  TreeNode* root1 = new TreeNode(3, node9, node20);
 
-  int result = sol.diameterOfBinaryTree(root);
+  bool result1 = sol.isBalanced(root1);
+  cout << "Tree [3,9,20,null,null,15,7] is balanced: " << (result1 ? "true" : "false") << endl;
 
-  cout << "res: " << result << endl;
+  TreeNode* node4a = new TreeNode(4);
+  TreeNode* node4b = new TreeNode(4);
+  TreeNode* node3a = new TreeNode(3, node4a, node4b);
+  TreeNode* node3b = new TreeNode(3);
+  TreeNode* node2a = new TreeNode(2, node3a, node3b);
+  TreeNode* node2b = new TreeNode(2);
+  TreeNode* root2 = new TreeNode(1, node2a, node2b);
 
-  delete root->left->left;
-  delete root->left->right;
-  delete root->left;
-  delete root->right;
-  delete root;
+  bool result2 = sol.isBalanced(root2);
+  cout << result2 ? "true" : "false") << endl;
 
   return 0;
 }
